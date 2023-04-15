0427e6446fbdab2cd6e3f58bce8270fa0646d20d
OmrM <omar12726@gmail.com>
2023-04-15 02:16:57 -0600

    edited linkDetection. moved validation to new method. closing connection. added try/catches

--- docDataValidator/src/main/java/io/jenkins/plugins/docDataValidator/LinkDetection.java
+++ docDataValidator/src/main/java/io/jenkins/plugins/docDataValidator/LinkDetection.java
@@ -7,46 +7,59 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class LinkDetection {
-    public static void main(ArrayList<String> locatedURLs, TaskListener listener) throws IOException {
-        if(locatedURLs.size() != 0)
-        {
-            System.out.println("Validating Links..." + "\n");
-            for(String urls: locatedURLs)
-            {
-                String codeStatus = " ";
-                URL url = new URL(urls);
-                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.connect();
-                int code = connection.getResponseCode();
+    public static void main(ArrayList<String> locatedURLs, TaskListener listener) {
+        if (locatedURLs.size() != 0) {
+            System.out.println("Validating Links...\n");
+            for (String urls : locatedURLs) {
+                // Check if the URL is HTTP or HTTPS (Change: Added check for HTTP/HTTPS URLs)
+                if (urls.startsWith("http://") || urls.startsWith("https://")) {
+                    try {
+                        // Change: Moved validation logic to a validateUrl Method
+                        validateUrl(urls, listener);
+                    } catch (IOException e) {
+                        listener.getLogger().println("Error processing URL: " + urls + ". " + e.getMessage());
+                    }
+                } else {
+                    listener.getLogger().println("Skipping non-HTTP/HTTPS URL: " + urls);
+                }
+            }
+        } else {
+            listener.getLogger().println("No links were detected in the file");
+        }
+    }
 
-                if(code >= 100 && code < 200)
-                {
-                    codeStatus = "informational response";
-                }
-                if(code >= 200 && code < 300)
-                {
-                    codeStatus = "Successful Response";
-                }
-                if(code >= 300 && code < 400)
-                {
-                    codeStatus = "Redirection";
-                }
-                if(code >= 400 && code < 500)
-                {
-                    codeStatus = "Client Error";
-                }
-                if(code >= 500 && code < 600)
-                {
-                    codeStatus = "Server Error";
-                }
-                listener.getLogger().println(urls + " " + code + " " + codeStatus);
+    // Change: Added new method to validate URLs
+    private static void validateUrl(String urlStr, TaskListener listener) throws IOException {
+        String codeStatus;
+        URL url = new URL(urlStr);
+        HttpURLConnection connection = null;
+
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int code = connection.getResponseCode();
+
+            if (code >= 100 && code < 200) {
+                codeStatus = "informational response";
+            } else if (code >= 200 && code < 300) {
+                codeStatus = "Successful Response";
+            } else if (code >= 300 && code < 400) {
+                codeStatus = "Redirection";
+            } else if (code >= 400 && code < 500) {
+                codeStatus = "Client Error";
+            } else if (code >= 500 && code < 600) {
+                codeStatus = "Server Error";
+            } else {
+                codeStatus = "Unknown Status";
+            }
+
+            listener.getLogger().println(urlStr + " " + code + " " + codeStatus);
+        } finally {
+            // Change: Added code to safely close the connection
+            if (connection != null) {
+                connection.disconnect();
             }
         }
-        else
-        {
-            listener.getLogger().println("No links we detected in the file");
-        }
-
     }
 }
